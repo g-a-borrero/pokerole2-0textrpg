1b043b124b762cb6ea18695b7bf2148ca38e86d1
Gabrielle <31347690+gabartomeo@users.noreply.github.com>
2023-08-27 19:31:08 -0400

    Update main.py
    Simplified the main file a bit

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import re
-
-from pokedex import pokedex as pdex
-from pokemon import Pokemon as pkmn
 from command_parser import parse_command
 
 game = True
 current_room = 0
 
+print("Let's journey through the world of Pokemon!")
 while game:
 	game, current_room = parse_command(input("> "), current_room)
